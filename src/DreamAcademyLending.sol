8cb29d096e7f35244eec7751cc62b533d3a95bd3
siwon-huh <shewon.heo@dream-academy.io>
2023-03-20 15:21:20 +0900

    initial commit

---
+++ src/DreamAcademyLending.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+
+
+interface IPriceOracle {
+    function getPrice(address asset) external view returns (uint256);
+}
+contract DreamAcademyLending is IPriceOracle{
+
+    IPriceOracle oracle;
+    address asset;
+    constructor(IPriceOracle _oracle, address _asset) {
+        oracle = _oracle;
+        asset = _asset;
+    }
+    function getPrice(address _asset) public view returns (uint256){
+
+    }
+    function initializeLendingProtocol(address) public payable {
+
+    }
+    function deposit(address tokenAddress, uint256 amount) public payable {
+
+    }
+
+    function borrow(address tokenAddress, uint256 amount) public {
+
+    }
+
+    function repay(address tokenAddress, uint256 amount) public {
+
+    }
+
+    function liquidate(address user, address tokenAddress, uint256 amount) public {
+
+    }
+
+
+    function withdraw(address tokenAddress, uint256 amount) external {
+
+    }
+
+    function getAccruedSupplyAmount(address _asset) public returns (uint256 accruedSupplyAmount) {
+
+    }
+
+    receive() external payable {
+        // for ether receive
+    }
+}
